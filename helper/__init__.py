84b4a38e368f247a6a175c08441a378db484c5e0
Toni Lukkaroinen <toni.lukkaroinen@hoosat.fi>
2024-03-07 18:57:08 +0200

    Deflationary table

--- helper/__init__.py
+++ helper/__init__.py
@@ -4,15 +4,15 @@ from cachetools.func import ttl_cache
 
 
 @ttl_cache(ttl=60)
-def get_kas_price():
+def get_htn_price():
     resp = requests.get("https://api.coingecko.com/api/v3/simple/price",
-                        params={"ids": "kaspa",
+                        params={"ids": "htn",
                                 "vs_currencies": "usd"})
     if resp.status_code == 200:
         return resp.json()["kaspa"]["usd"]
 
 @ttl_cache(ttl=60)
-def get_kas_market_data():
-    resp = requests.get("https://api.coingecko.com/api/v3/coins/kaspa")
+def get_htn_market_data():
+    resp = requests.get("https://api.coingecko.com/api/v3/coins/htn")
     if resp.status_code == 200:
         return resp.json()["market_data"]
